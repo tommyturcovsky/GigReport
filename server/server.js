37d9edfce4ec9451231af0fdf7fc35e071fb72a4
unknown <tommyturcovsky@gmail.com>
2020-04-24 20:01:34 -0400

    heroku mongodb support

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const cookieParse = require('cookie-parser')
 
 // This is the default address for MongoDB.
 // Make sure MongoDB is running!
-const mongoEndpoint = 'mongodb://127.0.0.1/gigReport_app';
+const mongoEndpoint = process.env.MONGODB_URI ||'mongodb://127.0.0.1/gigReport_app';
 // useNewUrlParser is not required, but the old parser is deprecated
 mongoose.connect(mongoEndpoint, { useNewUrlParser: true });
 // Get the connection string
